573879cdd1e237978c755a820089df34c3ac856c
Jaro Reinders <jaro.reinders@gmail.com>
2016-07-24 10:58:16 +0200

    Added tests (and fixed Yi.CompletionTree so they pass)

--- yi-core/src/Yi/CompletionTree.hs
+++ yi-core/src/Yi/CompletionTree.hs
@@ -18,13 +18,15 @@ Intended to be imported qualified:
 -}
 module Yi.CompletionTree (
   -- * CompletionTree type
-  CompletionTree (),
+  CompletionTree (CompletionTree),
   -- * Lists
   fromList, toList,
   -- * Modification
-  complete, update, updateOne,
+  complete, update,
   -- * Debugging
-  pretty
+  pretty,
+  -- ** Lens
+  unCompletionTree
   ) where
 
 import           Control.Arrow       (first)
@@ -35,32 +37,50 @@ import           Data.Map.Strict     (Map)
 import           Data.Maybe          (isJust, fromJust, listToMaybe, catMaybes)
 import qualified Data.ListLike       as LL
 import           Data.ListLike       (ListLike)
-import           Lens.Micro.Platform (over, makeLenses, Lens')
+import           Lens.Micro.Platform (over, Lens', _2, (.~), (&))
 
 -- | A CompletionTree is a map of partial completions.
-newtype CompletionTree a = CompletionTree {_completionTree :: (Map a (CompletionTree a))}
-  deriving (Monoid)
+--
+-- Example:
+--
+-- fromList ["put","putStr","putStrLn","print","abc"]
+--
+-- Gives the following tree:
+--
+--            / \
+--          "p" "abc"
+--         /  \
+--      "ut"  "rint"
+--      /  \
+--   "Str"  ""
+--   /  \
+-- "Ln"  ""
+--
+-- (The empty strings are needed to denote the end of a word)
+-- (A CompletionTree is not limited to a binary tree)
+newtype CompletionTree a = CompletionTree {_unCompletionTree :: (Map a (CompletionTree a))}
+  deriving (Monoid, Eq)
 
-completionTree :: Lens' (CompletionTree a) (Map a (CompletionTree a))
-completionTree f ct = (\completionTree' -> ct {_completionTree = completionTree'}) <$>
-                        f (_completionTree ct)
+unCompletionTree :: Lens' (CompletionTree a) (Map a (CompletionTree a))
+unCompletionTree f ct = (\unCompletionTree' -> ct {_unCompletionTree = unCompletionTree'}) <$>
+                        f (_unCompletionTree ct)
 
-instance (Show a, ListLike a i) => Show (CompletionTree a) where
+instance (Ord a, Show a, ListLike a i) => Show (CompletionTree a) where
   show ct = "fromList " ++ show (toList ct)
 
 -- | This function converts a list of completable elements to a CompletionTree
 -- It finds elements that share a common prefix and groups them.
 --
 -- prop> fromList . toList = id
-fromList :: (Ord a, ListLike a i, Show a, Eq i) => [a] -> CompletionTree a
+fromList :: (Ord a, ListLike a i, Eq i) => [a] -> CompletionTree a
 fromList [] = mempty
 fromList (x:xs)
-  | x == mempty = over completionTree (M.insert mempty mempty) (fromList xs)
+  | x == mempty = over unCompletionTree (M.insert mempty mempty) (fromList xs)
   | otherwise = case maximumBy' (compare `on` childrenIn xs) (tail $ LL.inits x) of
-      Nothing -> over completionTree (M.insert x mempty) (fromList xs)
+      Nothing -> over unCompletionTree (M.insert x mempty) (fromList xs)
       Just parent -> case first (x:) $ partition (parent `LL.isPrefixOf`) xs of
-        ([_],rest) -> over completionTree (M.insert parent mempty) $ fromList rest
-        (hasParent, rest) -> over completionTree (M.insert parent (fromList $
+        ([_],rest) -> over unCompletionTree (M.insert parent mempty) $ fromList rest
+        (hasParent, rest) -> over unCompletionTree (M.insert parent (fromList $
            map (fromJust . LL.stripPrefix parent) hasParent)) $ fromList rest
       -- A parent is the prefix and the children are the items with the parent as prefix
       where childrenIn list parent = length $ filter (parent `LL.isPrefixOf`) list
@@ -85,14 +105,18 @@ maximumBy' cmp l | atleast 2 (== max') l = Nothing
 -- >>> complete $ fromList ["put","putStrLn","putStr"]
 -- ("put", fromList ["","Str","StrLn"])
 --
--- >>> complete $ fromList ["put","putStr","putStrLn","xxx"]
--- ("", fromList ["put","putStr","putStrLn","xxx"])
-complete :: ListLike a i => CompletionTree a -> (a, CompletionTree a)
+-- >>> complete $ fromList ["put","putStr","putStrLn","abc"]
+-- ("", fromList ["put","putStr","putStrLn","abc"])
+complete :: (Eq i, Ord a, ListLike a i) => CompletionTree a -> (a, CompletionTree a)
 complete (CompletionTree ct)
-  | M.size ct == 1 = M.elemAt 0 ct
+  | M.size ct == 1 = if snd (M.elemAt 0 ct) == mempty
+                       then M.elemAt 0 ct & _2 .~ fromList [mempty]
+                       else M.elemAt 0 ct
   | otherwise = (mempty,CompletionTree ct)
 
 -- | Update the CompletionTree with new information.
+-- An empty list means that there is no completion left.
+-- A [mempty] means that the end of a word is reached.
 --
 -- Examples:
 --
@@ -104,11 +128,20 @@ complete (CompletionTree ct)
 --
 -- >>> update (fromList ["put","putStr"]) "putS"
 -- fromList ["tr"]
+--
+-- >>> update (fromList ["put"]) "find"
+-- fromList []
+--
+-- >>> update (fromList ["put"]) "put"
+-- fromList [""]
 update :: (Ord a, ListLike a i, Eq i) => CompletionTree a -> a -> CompletionTree a
 update (CompletionTree ct) p
-  -- p is empty:
-  | mempty == p = CompletionTree ct
-  -- p is a key in the map ct:
+  -- p is empty, this case just doesn't make sense:
+  | mempty == p = error "Can't update a CompletionTree with a mempty"
+  -- p is a key in the map ct that doesn't have children:
+  -- (This means the end of a word is reached)
+  | isJust one && mempty == fromJust one = CompletionTree $ M.singleton mempty mempty
+  -- p is a key in the map ct with children:
   | isJust one = fromJust one
   -- a substring of p is a key in ct:
   | isJust remaining = uncurry update $ fromJust remaining
@@ -121,26 +154,33 @@ update (CompletionTree ct) p
     remaining = listToMaybe . catMaybes $
       map (\p' -> (,fromJust $ LL.stripPrefix p' p) <$> M.lookup p' ct) (tail $ LL.inits p)
 
--- | Like 'update' but with only one item.
-updateOne :: (Ord a, ListLike a i, Eq i) => CompletionTree a -> i -> CompletionTree a
-updateOne (CompletionTree ct) p
-  -- (LL.singleton p) is a key in ct:
-  | isJust one = fromJust one
-  -- p is the head of a key in ct:
-  | otherwise = CompletionTree $ M.mapKeys fromJust
-                               $ M.filterWithKey (const . isJust)
-                               $ M.mapKeys (LL.stripPrefix $ LL.singleton p) ct
-  where
-    one = M.lookup (LL.singleton p) ct
-
 -- | Converts a CompletionTree to a list of completions.
 --
--- prop> toList . fromList = id
-toList :: ListLike a i => CompletionTree a -> [a]
-toList (CompletionTree ct)
-  | M.null ct = [mempty]
-  | otherwise = concat $ M.elems $ M.mapWithKey (\k v -> map (k `LL.append`) $ toList v) ct
+-- prop> toList . fromList = sort . nub
+--
+-- Examples:
+--
+-- >>> toList mempty
+-- []
+--
+-- >>> toList (fromList ["a"])
+-- ["a"]
+--
+-- >>> toList (fromList ["a","a","a"])
+-- ["a"]
+--
+-- >>> toList (fromList ["z","x","y"])
+-- ["x","y","z"]
+toList :: (Ord a, ListLike a i) => CompletionTree a -> [a]
+toList ct
+  | mempty == ct = []
+  | otherwise = toList' ct
+  where
+    toList' (CompletionTree ct)
+      | M.null ct = [mempty]
+      | otherwise = concat $ M.elems $ M.mapWithKey (\k v -> map (k `LL.append`) $ toList' v) ct
 
+-- TODO: make this function display a tree and rename to showTree
 -- | For debugging purposes.
 --
 -- Example:
